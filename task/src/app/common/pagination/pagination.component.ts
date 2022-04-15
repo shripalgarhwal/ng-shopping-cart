eb321fa32b0c21752a4cb4c5ea0461b70d192d84
Shripal <shripalgarhwal@users.noreply.github.com>
2022-04-15 19:01:06 +0530

    Add files via upload

---
+++ task/src/app/common/pagination/pagination.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+  pageNo: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChange = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngOnChanges() {
+    this.pageNo = 1;
+  }
+  get isFirst(): boolean {
+    return this.pageNo === 1;
+  }
+
+  get isLast(): boolean {
+    return this.pageNo === this.totalPages;
+  }
+  prev(): void {
+    this.setPage(Math.max(1, this.pageNo - 1));
+
+  }
+  next(): void {
+    this.setPage(Math.min(this.totalPages, this.pageNo + 1));
+  }
+  private setPage(val: number) {
+    this.pageNo = val;
+    this.pageChange.emit(this.pageNo);
+  }
+
+}
